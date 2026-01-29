c6aa6a6d5455eb37f99a0b16afa93094f003f033
Tetsu0822 <111884217+Tetsu0822@users.noreply.github.com>
2026-01-30 00:04:31 +0800

    feat:Route Setting

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,8 +4,16 @@ import Home from "../pages/front/Home";
 import NotFound from "../pages/front/NotFound";
 import Products from "../pages/front/Products";
 import SingleProduct from "../pages/front/SingleProduct";
+import BackendLayout from "../layout/BackendLayout";
+import Orders from "../pages/admin/Orders";
+import Dashboard from "../pages/admin/Dashboard";
+import AdminProducts from "../pages/admin/AdminProducts";
+import AdminSingleProduct from "../pages/admin/AdminSingleProduct";
 
+
+// 前後台 Routes 分離
 const routes = [
+    // 前台路由
     {
         path: '/',
         element: <FrontendLayout />,
@@ -29,6 +37,29 @@ const routes = [
             {
                 path: '*',
                 element: <NotFound />
+            }
+        ]
+    },
+    // 後台路由
+    {
+        path: '/admin',
+        element: <BackendLayout />,
+        children: [
+            {
+                index: true,
+                element: <Dashboard />
+            },
+            {
+                path: 'products',
+                element: <AdminProducts />
+            },
+            {
+                path: 'products/:id',
+                element: <AdminSingleProduct />
+            },
+            {
+                path: 'orders',
+                element: <Orders />
             }
         ]
     }
